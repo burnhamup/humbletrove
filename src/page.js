9454d2ba960d7c517f453cec4e68cc53c5eea810
Chris <chris@burnhamup.com>
2025-01-20 07:37:35 -0800

    cache catalog data in localstorage

--- src/page.js
+++ src/page.js
@@ -1,8 +1,25 @@
 // TODO cache in localstorage
-const games = {};
+
+const loadCatalog = async () => {
+  let games = {}
+  let result = await chrome.storage.local.get(['catalog']);
+  let cachedObject = result.catalog;
+  debugger;
+  if (!cachedObject || new Date().getTime() < cachedObject.expiry) {
+    games = cachedObject.data;
+  } else {
+    games = await fetchCatalog();
+    await chrome.storage.local.set({'catalog': {
+      'data': games,
+      'expiry': new Date().getTime() + 7 * 24 * 60 * 60 * 1000,
+    }});
+  }
+  renderGames(games);
+}
 
 const fetchCatalog = async () => {
   const MAX_PAGES = 4;
+  let games = {}
   // TODO I can probably do this in parallel.
   for (let i=0; i<MAX_PAGES; i++) {
     const response = await fetch(`https://www.humblebundle.com/client/catalog?index=${i}`, {
@@ -11,34 +28,36 @@ const fetchCatalog = async () => {
     const data = await response.json();
     data.forEach((game) => {
       // Parse data
-      games[game.machine_name] = {
-        machine_name: game.machine_name,
-        human_name: game['human-name'],
-        downloads: Object.entries(game.downloads).map(([platform, data]) => {
-          return {
-            platform: platform,
-            filename: data.url.web,
-            machine_name: data.machine_name,
-          }
-        }),
-        // TODO copy more things
-      }
+      games[game.machine_name] = game;
     })
   }
-  renderGames();
+  return games;
 }
 
-const renderGames = () => {
+const renderGames = (games) => {
   const catalogList = document.getElementById("catalog-list");
   const gamesHtml = Object.values(games).map(renderGame).join('');
   catalogList.innerHTML = gamesHtml;
 }
 
 const renderGame = (game) => {
+   let gameDict = {
+    machine_name: game.machine_name,
+    human_name: game['human-name'],
+    downloads: Object.entries(game.downloads).map(([platform, data]) => {
+      return {
+        platform: platform,
+        filename: data.url.web,
+        machine_name: data.machine_name,
+      }
+    }),
+    // TODO copy more things
+  };
+
   return `
-    <li>${game.human_name}
+    <li>${gameDict.human_name}
     <ul>
-      ${game.downloads.map(download => `<li><button class="js-download" data-filename="${download.filename}" data-machinename="${download.machine_name}">${download.platform}</button></li>`)}
+      ${gameDict.downloads.map(download => `<li><button class="js-download" data-filename="${download.filename}" data-machinename="${download.machine_name}">${download.platform}</button></li>`)}
     </ul>
     </li>
   `
@@ -69,7 +88,7 @@ document.getElementById("fetch-data").addEventListener("click", async () => {
   const catalogList = document.getElementById("catalog-list");
   catalogList.innerHTML = "<li>Loading...</li>";
   try {
-    fetchCatalog();
+    loadCatalog();
   } catch (error) {
     catalogList.innerHTML = `<li>Error: ${error.message}</li>`;
   }
