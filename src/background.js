b95ecdfb6f3d09c73f3160d7ffb7e1b945fbc9e3
Chris <chris@burnhamup.com>
2025-01-20 08:20:09 -0800

    restricting permissions

--- src/background.js
+++ src/background.js
@@ -1,6 +1,5 @@
 chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({
-      url: chrome.runtime.getURL("page.html")
-    });
+  chrome.tabs.create({
+    url: chrome.runtime.getURL("page.html")
   });
-  
+});
